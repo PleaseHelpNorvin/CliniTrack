d549aad54ffa6f82978d621fb0e1651d97076100
PleaseHelpNorvin <norvincrujido90@gmail.com>
2025-11-07 23:52:27 +0800

    preparing for nurse students

--- app/Http/Controllers/Nurse/StudentController.php
+++ app/Http/Controllers/Nurse/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Nurse;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
@@ -11,4 +12,18 @@ class StudentController extends Controller
     public function index() {
         return view('nurse_pages.student_pages.index');
     }
+
+    public function create() {
+
+    }
+
+    public function store() {
+        
+    }
+
+    public function view(Student $student) {
+        $student->load(['visits.nurse', 'documents']);
+        return view('nurse_pages.student_pages.view', compact('student'));
+    }
+
 }
